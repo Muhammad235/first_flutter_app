f1ec79e609b7b9e3da41e238154f97cf5882ad33
Muhammad Yahaya <adelekeyahaya05@gmail.com>
2024-08-25 07:40:51 +0100

    update: app screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,35 @@
 import 'package:flutter/material.dart';
+import 'package:english_words/english_words.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSwatch(primarySwatch: Colors.purple),
-      ),
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('WordPair Generator'),
-        ),
-        body: Center(
-          child: Text('Hello'),
+        appBarTheme: const AppBarTheme(
+          backgroundColor: Colors.purple // Apply color to the AppBar
         ),
       ),
+      home: const Home()
+    );
+  }
+}
+
+class Home extends StatelessWidget {
+  const Home({super.key});
+  @override
+  Widget build(BuildContext context)
+  {
+    final newWordPair = WordPair.random();
+
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('MyApp'),
+      ),
+        body: const Center(child: Text ("First App"))
     );
   }
 }
