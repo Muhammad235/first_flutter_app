b12b0c9541bb5922b176276028d0cc9c3c5dfa85
Muhammad Yahaya <adelekeyahaya05@gmail.com>
2024-09-21 21:59:17 +0100

    add: onboarding screen

--- lib/0_nexio_app/onboarding/onboarding1.dart
+++ lib/0_nexio_app/onboarding/onboarding1.dart
@@ -5,14 +5,122 @@ class Onboarding1 extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-        title: "Flutter App",
-        debugShowCheckedModeBanner: false,
-        home: Scaffold(
-          body: Center(child: Text("Onboarding page")),
-        )
+    return MaterialApp(
+      title: "Flutter App",
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            Stack(
+              children: [
+                Image.asset(
+                  "images/frame.png",
+                  width: double.infinity, //full width
+                  height: 500,
+                  fit: BoxFit.cover, // Ensures it covers the full width
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(left: 25.0, top: 100),
+                  child: Image.asset(
+                    "images/card1.png",
+                    width: 300,
+                    height: 300,
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(left: 85.0, top: 150),
+                  child: Image.asset(
+                    "images/card2.png",
+                    width: 300,
+                    height: 300,
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(left: 175.0, top: 220),
+                  child: Image.asset(
+                    "images/card3.png",
+                    width: 300,
+                    height: 300,
+                  ),
+                ),
+              ],
+            ),
+            Container(
+              padding: const EdgeInsets.only(left: 30.7, bottom: 50),
+              child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    const Text(
+                      "Ready to take",
+                      style: TextStyle(
+                        fontSize: 54.0,
+                        fontWeight: FontWeight.w400,
+                      ),
+                    ),
+                    const Text(
+                      "your money beyond",
+                      style: TextStyle(
+                          fontSize: 54.0,
+                          color: Colors.deepPurple,
+                          fontWeight: FontWeight.w400,
+                          height: 0.999),
+                    ),
+                    // const SizedBox(height: 0),
+                    const Row(
+                      children: [
+                        Text(
+                          "borders",
+                          style: TextStyle(
+                              fontSize: 54.0,
+                              color: Colors.deepPurple,
+                              fontWeight: FontWeight.w400),
+                        ),
+                        Text(
+                          "?",
+                          style: TextStyle(
+                            fontSize: 54.0,
+                          ),
+                        ),
+                      ],
+                    ),
+                    const SizedBox(height: 15),
+                    ElevatedButton.icon(
+                      onPressed: () {},
+                      label: const Text("Get Started"),
+                      style: ElevatedButton.styleFrom(
+                        padding: const EdgeInsets.all(0),
+                        backgroundColor:
+                            Colors.black, // Button background color
+                        fixedSize: const Size(400, 75), // Button size
+                        textStyle: const TextStyle(
+                          fontSize: 25,
+                          // fontWeight: FontWeight.bold,
+                        ),
+                        foregroundColor: Colors.white, // Text color
+                        alignment: Alignment.center,
+                      ),
+                    ),
+                    const SizedBox(height: 22.5),
+                    const Row(
+                      // padding: EdgeInsets.only(top: 10.7),
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text("I already have a Nexio account. ",
+                            style: TextStyle(
+                                fontSize: 18, fontWeight: FontWeight.w400)),
+                        Text("Sign me in",
+                            style: TextStyle(
+                                fontSize: 18,
+                                color: Colors.deepPurple,
+                                fontWeight: FontWeight.w500)),
+                      ],
+                    )
+                  ]),
+            )
+          ],
+        ),
+      ),
     );
   }
 }
-
-
